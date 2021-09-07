137f1547e44c3898f9d07301f2f54f5a89ff6038
Renato Tripi <renatosft@gmail.com>
2021-09-06 23:05:09 -0300

    Added smooth scroll function

--- script.js
+++ script.js
@@ -20,17 +20,49 @@ function initTabNav() {
 }
 initTabNav();
 
-const accordionList = document.querySelectorAll(".js-accordion dt");
-const activeClass = "ativo";
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
 
-accordionList[0].classList.add(activeClass);
-accordionList[0].nextElementSibling.classList.add(activeClass);
+  accordionList[0].classList.add(activeClass);
+  accordionList[0].nextElementSibling.classList.add(activeClass);
 
-function activeAccordion() {
-  //console.log(this);
-  this.classList.toggle(activeClass);
-  this.nextElementSibling.classList.toggle(activeClass);
+  function activeAccordion() {
+    //console.log(this);
+    this.classList.toggle(activeClass);
+    this.nextElementSibling.classList.toggle(activeClass);
+  }
+  accordionList.forEach((item) => {
+    item.addEventListener("click", activeAccordion);
+  });
 }
-accordionList.forEach((item) => {
-  item.addEventListener("click", activeAccordion);
-});
+
+initAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+
+    //alternativa
+    // window.scrollTo({
+    //   top:topo,
+    //   behavior:"smooth"
+    // })
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+initScrollSuave();
