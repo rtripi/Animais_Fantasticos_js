86129209cca30ad2870e0b68275258f8c8854cf9
Renato Tripi <renatosft@gmail.com>
2021-09-06 22:22:48 -0300

    tab navigation to animals section

---
+++ script.js
@@ -0,0 +1,18 @@
+const tabMenu = document.querySelectorAll(".js-tabmenu li"); //para selecionar cada item
+const tabContent = document.querySelectorAll(".js-tabcontent section");
+tabContent[0].classList.add("ativo");
+
+if (tabMenu.length && tabContent.length) {
+  function activeTab(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove("ativo");
+    });
+    tabContent[index].classList.add("ativo");
+  }
+
+  tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener("click", function () {
+      activeTab(index);
+    });
+  });
+}
