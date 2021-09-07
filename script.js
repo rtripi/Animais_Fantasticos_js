77900a10f3ae58ae8d544e5ddf13b2c5486a0c43
Renato Tripi <renatosft@gmail.com>
2021-09-06 23:22:08 -0300

    Added scroll animation

--- script.js
+++ script.js
@@ -66,3 +66,27 @@ function initScrollSuave() {
 }
 
 initScrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        if (sectionTop < 0) {
+          // <0 [e o numero que da quando ele passa do topo da pagina]
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+
+initAnimacaoScroll();
