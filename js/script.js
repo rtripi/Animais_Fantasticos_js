2b3dc57a6d14038dcb291e6427563029d3a50df9
Renato Tripi <renatosft@gmail.com>
2021-10-18 18:52:24 -0300

    accordion transformado em classe/ accordion turned into a class

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 import ScrollSuave from "./modules/scroll_suave.js";
+import Accordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
-import initAccordion from "./modules/accordion.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
-
 import initfuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetchAnimais.js";
 import initFetchBTC from "./modules/fetchBtc.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
+
+const accordion = new Accordion(".js-accordion dt");
+accordion.init();
+
 initTabNav();
 
-initAccordion();
 initAnimacaoScroll();
 
 initModal();
