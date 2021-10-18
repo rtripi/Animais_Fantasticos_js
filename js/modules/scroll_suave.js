6e593bf96269593b0848a5c7e04ccaea2a3a4380
Renato Tripi <renatosft@gmail.com>
2021-10-18 18:21:28 -0300

    scroll_suave transformado em class/smooth scroll turned into a class

--- js/modules/scroll_suave.js
+++ js/modules/scroll_suave.js
@@ -1,10 +1,21 @@
-export default function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: "smooth", block: "start" };
+    } else {
+      this.options = options;
+    }
 
-  function scrollToSection(event) {
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(event) {
     event.preventDefault();
+
     const href = event.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
+    console.log(section);
     // const topo = section.offsetTop;
 
     // alternativa
@@ -13,13 +24,19 @@ export default function initScrollSuave() {
     //   behavior:"smooth"
     // })
 
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
+    section.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener("click", this.scrollToSection);
     });
   }
 
-  linksInternos.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-  });
+  init() {
+    if (this.linksInternos.length) {
+      this.addLinkEvent();
+      return this;
+    }
+  }
 }
